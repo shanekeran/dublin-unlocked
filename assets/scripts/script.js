498733cb364fcf148e187c86751a3f3c05a17682
shanekeran <shanekeran@gmail.com>
2021-01-19 16:52:25 +0000

    Added home page background images and jQuery toggler button for day/night theme

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,5 +3,10 @@ $( document ).ready(function() {
     $("#enter-button").on("click", function() {
         $("#splash-page").hide();
         $("#main-content").css("display","block");
-    });
+    }); 
+    $("#time-button").on("click", function(){
+        $("#home").toggleClass("home-image-night home-image-day");
+        $("#time-button").toggleClass("day-button night-button");
+        $("#button-text").text($("#button-text").text() == "Night" ? "Day":"Night");
+    }); 
 });
