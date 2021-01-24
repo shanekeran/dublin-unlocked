b3dc120e608e2ac8d5f1b66f2af7ec0e5e9c1941
shanekeran <shanekeran@gmail.com>
2021-01-24 21:28:12 +0000

    Added night time images to each section with jQuery

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,8 +5,13 @@ $( document ).ready(function() {
         $("#main-content").css("display","block");
     }); 
     $("#time-button").on("click", function(){
-        $("#home-image").toggleClass("home-image-night home-image-day");
         $("#time-button").toggleClass("day-button night-button");
         $("#button-text").text($("#button-text").text() == "Night" ? "Day":"Night");
+        // Image changes Day / Night
+        $("#home-image").toggleClass("home-image-night home-image-day");
+        $("#attractions-image").toggleClass("attractions-image-night attractions-image-day");
+        $("#events-image").toggleClass("events-image-night events-image-day");
+        $("#weather-image").toggleClass("weather-image-night weather-image-day");
+        $("#hotels-image").toggleClass("hotels-image-night hotels-image-day");
     }); 
 });
