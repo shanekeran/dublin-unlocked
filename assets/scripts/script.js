3e1e46a2c714783d84fa32b4302f1370ba7865e7
shanekeran <shanekeran@gmail.com>
2021-01-26 20:57:40 +0000

    Added Google Maps API to Attractions section

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -33,6 +33,17 @@ $( document ).ready(function() {
     }); 
 });
 
+//-------------- Google Maps API ------------------ \\
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 53.350140, lng: -6.25972 },
+    zoom: 11,
+  });
+}
+
 //-------------- Weather API ------------------ \\
 var request = new XMLHttpRequest()
 
